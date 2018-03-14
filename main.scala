69d48add695919021e601a5aff1561d0282fc5eb
車大燃画 <quiye@users.noreply.github.com>
2018-03-14 22:13:15 +0900

    Update main.scala

--- main.scala
+++ main.scala
@@ -3,9 +3,14 @@ import org.msgpack.core.MessagePack
 object Tester {
   def main(args: Array[String]) = {
     val packer = MessagePack.newDefaultBufferPacker()
-    packer.packInt(999)
+    packer.packInt(777)
+    packer.packInt(888)
+    packer.packString("nine nine nine")
     packer.close()
     val unpacker = MessagePack.newDefaultUnpacker(packer.toByteArray())
     println(unpacker.unpackInt)
+    println(unpacker.unpackInt)
+    println(unpacker.unpackString)
+    unpacker.close()
   }
 }
